08bc90475ce636ce0686ef32aa572cafb29d80d6
saku <ayana-03101@outlook.jp>
2024-07-22 22:03:01 +0900

    バリデーション

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -10,7 +10,16 @@
   <div class="todo__alert--success">
     {{ session('message') }}
   </div>
-   @endif
+  @endif
+  @if ($errors->any())
+  <div class="todo__alert--danger">
+    <ul>
+      @foreach ($errors->all() as $error)
+      <li>{{ $error }}</li>
+      @endforeach
+    </ul>
+  </div>
+  @endif
 </div>
 
 <div class="todo__content">
