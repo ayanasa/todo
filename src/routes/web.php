80df53ac66094a50578d1bec74ff25d9597fa3d0
saku <ayana-03101@outlook.jp>
2024-07-23 21:30:05 +0900

    更新機能

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,3 +5,4 @@ use App\Http\Controllers\TodoController;
 
 Route::get('/', [TodoController::class, 'index']);
 Route::post('/todos', [TodoController::class, 'store']);
+Route::patch('/todos/update', [TodoController::class, 'update']);
