77d14a028ed4b7ce29d14bd6d2bdbe32d65000b3
saku <ayana-03101@outlook.jp>
2024-07-25 21:58:39 +0900

    バリデーション

--- src/app/Http/Controllers/CategoryController.php
+++ src/app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CategoryRequest;
 use App\Models\Category;
 use Illuminate\Http\Request;
 
@@ -13,7 +14,7 @@ class CategoryController extends Controller
 
         return view('category', compact('categories'));
     }
-    public function store(Request $request)
+    public function store(CategoryRequest $request)
     {
         $category = $request->only(['name']);
         Category::create($category);
